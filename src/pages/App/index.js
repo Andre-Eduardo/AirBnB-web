65c58db2eec09d0ecf7ad2f6ccc906568f819cb5
André Eduardo <andre.eduardo11@hotmail.com>
2019-10-23 10:37:59 -0300

    lista de imoveis

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -3,11 +3,21 @@ import Dimensions from "react-dimensions";
 import { Container } from "./styles";
 import MapGL from "react-map-gl";
 import PropTypes from "prop-types";
-
+import debounce from "lodash/debounce";
+import api from "../../services/api";
+import Properties from "./components/Properties";
 const TOKEN =
   "pk.eyJ1IjoiYW5kcmUtZWR1YXJkbyIsImEiOiJjazFydWJnNDMwOTQzM2NueXJzOHVobXJzIn0.3zXesN6jxrEtueZsKbnQYg";
 
 class Map extends Component {
+  constructor() {
+    super();
+    this.updatePropertiesLocalization = debounce(
+      this.updatePropertiesLocalization,
+      500
+    );
+  }
+
   static propTypes = {
     containerWidth: PropTypes.number.isRequired,
     containerHeight: PropTypes.number.isRequired
@@ -20,10 +30,33 @@ class Map extends Component {
       zoom: 12.8,
       bearing: 0,
       pitch: 0
+    },
+    properties: []
+  };
+
+  componentDidMount() {
+    this.loadProperties();
+  }
+
+  updatePropertiesLocalization() {
+    this.loadProperties();
+  }
+
+  loadProperties = async () => {
+    const { latitude, longitude } = this.state.viewport;
+    try {
+      const response = await api.get("/properties", {
+        params: { latitude, longitude }
+      });
+      this.setState({ properties: response.data });
+    } catch (err) {
+      console.log(err);
     }
   };
+
   render() {
     const { containerWidth: width, containerHeight: height } = this.props;
+    const { properties } = this.state;
     return (
       <MapGL
         width={width}
@@ -32,7 +65,10 @@ class Map extends Component {
         mapStyle="mapbox://styles/mapbox/dark-v9"
         mapboxApiAccessToken={TOKEN}
         onViewportChange={viewport => this.setState({ viewport })}
-      />
+        onViewStateChange={this.updatePropertiesLocalization.bind(this)}
+      >
+        <Properties properties={properties} />
+      </MapGL>
     );
   }
 }
